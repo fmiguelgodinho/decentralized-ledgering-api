97aed392d202fd6904425af59d80589d1baa3755
Francisco Godinho <f.miguelgodinho@gmail.com>
2018-06-16 20:53:36 +0100

    dto

--- src/core/ContractInterpreter.java
+++ src/core/ContractInterpreter.java
@@ -1,5 +1,18 @@
 package core;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import core.dto.Contract;
+
 public class ContractInterpreter {
+	
+	private Map<String, Contract> contracts;	// list of contracts loaded in memory (CID -> contract)
+												// if a contract isn't here, this interpreter will fetch it from the BC
+	
+	
+	public ContractInterpreter() {
+		contracts = new HashMap<String, Contract>();
+	}
 
 }
---
+++ src/core/dto/Contract.java
@@ -0,0 +1,23 @@
+package core.dto;
+
+import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class Contract {
+	
+	
+	private Map<String, Object> attributes;
+	
+	
+	public Contract() {
+		
+	}
+	
+	
+	public JsonObject toJson() {
+		return null;
+	}
+
+}
