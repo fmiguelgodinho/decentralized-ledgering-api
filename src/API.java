9091a457ec4c1c47d3b888768d7c92579d3af144
Francisco Godinho <f.miguelgodinho@gmail.com>
2018-07-22 19:06:24 +0100

    peer connections by cfg

--- src/API.java
+++ src/API.java
@@ -54,16 +54,7 @@ public class API {
 	public static final String CONFIG_FILEPATH = "conf/config.properties";
 	
 	// TODO: nodes can be specified on contract
-	public static NodeConnection[] HLF_INTEGRATION_CHANNEL_NODES = new NodeConnection[] {
-		new NodeConnection(NodeConnection.PEER_TYPE, "peer0.blockchain-a.com", "localhost", 7051, "../../bootstrap/crypto-config/peerOrganizations/blockchain-a.com/tlsca/tlsca.blockchain-a.com-cert.pem"),
-		new NodeConnection(NodeConnection.PEER_TYPE, "peer0.blockchain-b.com", "localhost", 10051, "../../bootstrap/crypto-config/peerOrganizations/blockchain-b.com/tlsca/tlsca.blockchain-b.com-cert.pem"),
-		new NodeConnection(NodeConnection.PEER_TYPE, "peer0.blockchain-c.com", "localhost", 8051, "../../bootstrap/crypto-config/peerOrganizations/blockchain-c.com/tlsca/tlsca.blockchain-c.com-cert.pem"),
-		new NodeConnection(NodeConnection.PEER_TYPE, "peer0.blockchain-d.com", "localhost", 9051, "../../bootstrap/crypto-config/peerOrganizations/blockchain-d.com/tlsca/tlsca.blockchain-d.com-cert.pem"),
-		new NodeConnection(NodeConnection.PEER_TYPE, "peer0.blockchain-e.com", "localhost", 11051, "../../bootstrap/crypto-config/peerOrganizations/blockchain-e.com/tlsca/tlsca.blockchain-e.com-cert.pem"),
-		new NodeConnection(NodeConnection.PEER_TYPE, "peer0.blockchain-f.com", "localhost", 12051, "../../bootstrap/crypto-config/peerOrganizations/blockchain-f.com/tlsca/tlsca.blockchain-f.com-cert.pem"),
-		new NodeConnection(NodeConnection.ORDERER_TYPE, "orderer0.consensus.com", "localhost", 7050, "../../bootstrap/crypto-config/ordererOrganizations/consensus.com/tlsca/tlsca.consensus.com-cert.pem"),
-		new NodeConnection(NodeConnection.ORDERER_TYPE, "orderer1.consensus.com", "localhost", 8050, "../../bootstrap/crypto-config/ordererOrganizations/consensus.com/tlsca/tlsca.consensus.com-cert.pem")
-	};
+	public static NodeConnection[] HLF_INTEGRATION_CHANNEL_NODES;
 	
 	private static Configuration cfg;
 	private static ContractInterpreter ci;
@@ -92,6 +83,21 @@ public class API {
 		
 		// set the security provider...
 		Security.addProvider(new BouncyCastleProvider());
+		
+		// set up bootstrap nodes
+		
+		String[] bootstrapNodes = cfg.getStringArray("hlf.bootstrapNodes");
+		HLF_INTEGRATION_CHANNEL_NODES = new NodeConnection[bootstrapNodes.length];
+		for (int i = 0; i < bootstrapNodes.length; i++) {
+			String cfgNode = bootstrapNodes[i];
+			HLF_INTEGRATION_CHANNEL_NODES[i] = new NodeConnection(
+				cfg.getInt("bootstrapNode." + cfgNode + ".type"),
+				cfg.getString("bootstrapNode." + cfgNode + ".name"),
+				cfg.getString("bootstrapNode." + cfgNode + ".host"),
+				cfg.getInt("bootstrapNode." + cfgNode + ".port"),
+				cfg.getString("bootstrapNode." + cfgNode + ".caCrtPath")
+			);
+		}
 		
 	}
 	
